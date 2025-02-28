e396b74ff4044fbd4fb60f04dbea02c6ed05f810
Long Tran <plongtran239@gmail.com>
2025-02-28 11:14:36 +0700

    Implement register, apply repository pattern & nestjs-zod to validate

--- src/shared/constants/auth.constant.ts
+++ src/shared/constants/auth.constant.ts
@@ -14,3 +14,9 @@ export const ConditionGuard = {
 } as const;
 
 export type ConditionGuardType = (typeof ConditionGuard)[keyof typeof ConditionGuard];
+
+export const UserStatus = {
+  ACTIVE: 'ACTIVE',
+  INACTIVE: 'INACTIVE',
+  BLOCKED: 'BLOCKED',
+} as const;
