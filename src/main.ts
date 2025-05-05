3cce76d5478bc66da966e54cbb31d52127b6ecd1
Long Tran <plongtran239@gmail.com>
2025-05-05 12:18:40 +0700

    chore: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ import { setupSwagger } from 'src/swagger';
 import { WebSocketAdapter } from 'src/websocket/websocket.adapter';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: {
-      origin: '*',
-    },
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
   const webSocketAdapter = new WebSocketAdapter(app);
